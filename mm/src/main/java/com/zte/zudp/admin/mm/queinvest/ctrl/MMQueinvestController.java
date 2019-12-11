470fe03e8a8e1a5aceaf62fc46da88275bd56d62
hjm <hjm@qq.com>
2019-12-11 20:17:56 +0800

    invest

---
+++ mm/src/main/java/com/zte/zudp/admin/mm/queinvest/ctrl/MMQueinvestController.java
@@ -0,0 +1,118 @@
+package com.zte.zudp.admin.mm.queinvest.ctrl;
+
+import com.zte.zudp.admin.common.persistence.Subject;
+import com.zte.zudp.admin.common.security.SubjectUtil;
+import com.zte.zudp.admin.common.util.IDUtil;
+import com.zte.zudp.admin.info.queinvest.entity.Answers;
+import com.zte.zudp.admin.info.queinvest.entity.Queinvest;
+import com.zte.zudp.admin.mm.queinvest.entity.MMQueinvest;
+import com.zte.zudp.admin.mm.queinvest.service.MMQueinvestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+/**
+ * 公众调查
+ */
+
+@Controller
+@RequestMapping(value = "/mm/queinvest")
+public class MMQueinvestController {
+
+    @Autowired
+    private MMQueinvestService mmQueinvestService;
+
+
+    /**
+     * 问卷列表
+     * @param model
+     * @return
+     */
+    @RequestMapping("/survey")
+    public String survey(Model model) {
+        List<MMQueinvest> queinvestList=mmQueinvestService.selectAll();
+        model.addAttribute("queinvestList",queinvestList);
+        return "mm/news/survey";
+    }
+
+    /**
+     *
+     * @param model
+     * @return
+     */
+    @RequestMapping("/startQueinvest/{id}")
+    public String startQueinvest(Model model,@PathVariable("id")String id) {
+        List<MMQueinvest> list = mmQueinvestService.startQueinvest(id);
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getTitle() != null) {
+                String title = list.get(i).getTitle().toString();
+                model.addAttribute("title", title);
+            }
+        }
+            model.addAttribute("list", list);
+
+            return "mm/queinvest/queinvestDetail";
+        }
+
+
+
+
+    /**
+     * 得到答案 并且插入到数据库（答案表）中
+     * @param map
+     * @return
+     */
+    @GetMapping("/test")
+        public String test(@RequestParam Map<String ,Object> map) {
+        // 题目数量
+            int queSize = 0;
+            // 存放查询出来的结果
+            List<MMQueinvest> list= new ArrayList();
+            String str002 = map.get("1").toString();
+            try {
+                if (map.get("queSize") != null) { // 题目数量不为空
+                    queSize = Integer.parseInt((String) map.get("queSize"));
+
+                }
+                // 让题目数量为长度，进行遍历
+                for (int i = 1; i <= queSize; i++) {
+                    // 获得到每个被选中的单选按钮的值  选项内容
+                     if(map.get(i+"") != null){
+                         String str = map.get(i+"").toString();
+                         // 调用service层，查询出此答案所对应的题目和问卷
+                        MMQueinvest answer = mmQueinvestService.selectAllByAnswer(str);
+                        answer.setOptContext(str);
+                        // 将查询出来的结果放入到list中
+                        list.add(answer);
+                     }
+                }
+                System.out.println(list);
+                // 遍历list，将它插入到答案表中
+                for(int i = 0;i<list.size();i++){ //得到的问卷名称
+
+                    String queinvestId = list.get(i).getQueinvestId(); // 问卷id
+                    String questionId =  list.get(i).getQuestionId(); // 题目id
+                    String optContext = list.get(i).getOptContext(); // 所选的答案
+                    //生成一个无序的uuid
+                    String id = UUID.randomUUID().toString();
+                    // 将其插入到答案表中
+                    mmQueinvestService.insertToAnswer(id,queinvestId,questionId,optContext);
+
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+
+            return "/mm/news/index";
+
+        }
+
+
+
+}
---
+++ mm/src/main/java/com/zte/zudp/admin/mm/queinvest/dao/MMQueinvestDao.java
@@ -0,0 +1,44 @@
+package com.zte.zudp.admin.mm.queinvest.dao;
+
+import com.zte.zudp.admin.common.persistence.dao.AbstractDao;
+import com.zte.zudp.admin.info.queinvest.entity.Answers;
+import com.zte.zudp.admin.info.queinvest.entity.Queinvest;
+import com.zte.zudp.admin.mm.queinvest.entity.MMQueinvest;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 问卷调查
+ */
+
+
+
+public interface MMQueinvestDao extends AbstractDao<MMQueinvest> {
+
+
+    List<MMQueinvest> selectAll();
+
+    List<MMQueinvest> startQueinvest(@Param("id") String id);
+
+    /**
+     * 根据所选答案，查询出所有的信息
+     * @param str
+     * @return
+     */
+    MMQueinvest selectAllByAnswer(@Param("str") String str);
+
+    /**
+     * 将获得的答案 ，问卷id，题目id新增到答案表中
+     * @param queinvestId
+     * @param questionId
+     * @param optContext
+     */
+    void insertToAnswer(@Param("id") String id,@Param("queinvestId") String queinvestId, @Param("questionId") String questionId, @Param("optContext") String optContext);
+}
